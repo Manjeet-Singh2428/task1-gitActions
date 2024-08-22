a5eedd14f7e6b47bd865731236e4ff03435725d4
Manjeet-Singh2428 <108221122+Manjeet-Singh2428@users.noreply.github.com>
2024-08-22 18:28:21 +0530

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from hello import hello
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "Hello, World!")
+        self.assertEqual(hello(), "Hello, World in task 1!")
 
 if __name__ == "__main__":
     unittest.main()
